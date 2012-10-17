47989b1209ab7342d50242096090c3f0e6078fa1
Luca Abeni <luca.abeni@unitn.it>
2012-10-17 14:12:07 +0200

    Allow buinding 64bit kernels

--- monolithic.sh
+++ monolithic.sh
@@ -1,8 +1,24 @@
 set -e
 
+GUEST_CONFIG=config-3.4-guest-32
+HOST_CONFIG=config-3.4-host-32
+export HOST_ARCH=x86
+export GUEST_ARCH=x86
+
+while getopts 4 opt
+ do
+  case "$opt" in
+    4)		HOST_ARCH=x86_64; HOST_CONFIG=config-3.4-host-64;;
+    [?])	print >&2 "Usage: $0 [-4]"
+		exit 1;;
+  esac
+ done
+
 sh create_image.sh test.img 512 32
 sh create_image.sh opt1.img 48
-sh buildguest.sh $(pwd)/core.gz $(pwd)/Configs/config-3.4-guest-32 opt1.img test.img
+export ARCH=$GUEST_ARCH
+sh buildguest.sh $(pwd)/core.gz $(pwd)/Configs/$GUEST_CONFIG opt1.img test.img
 sh grubit.sh test.img
-sh buildhostlin.sh $(pwd)/core.gz $(pwd)/Configs/config-3.4-host-32 test.img
+export ARCH=$HOST_ARCH
+sh buildhostlin.sh $(pwd)/core.gz $(pwd)/Configs/$HOST_CONFIG test.img
 sh buildkvm.sh test.img
