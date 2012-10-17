744e597c60fecf434e7c2364faa0b3de31031d70
Luca Abeni <luca.abeni@unitn.it>
2012-10-17 13:30:29 +0200

    Rename config-*-host into ...-host-32

--- monolithic.sh
+++ monolithic.sh
@@ -4,5 +4,5 @@ sh create_image.sh test.img 512 32
 sh create_image.sh opt1.img 48
 sh buildguest.sh $(pwd)/core.gz $(pwd)/Configs/config-3.4-guest-32 opt1.img test.img
 sh grubit.sh test.img
-sh buildhostlin.sh $(pwd)/core.gz $(pwd)/Configs/config-3.4-host test.img
+sh buildhostlin.sh $(pwd)/core.gz $(pwd)/Configs/config-3.4-host-32 test.img
 sh buildkvm.sh test.img
