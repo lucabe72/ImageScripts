f7b6d6e1c069ff79c3bda7fef60b430901a2e08d
luca abeni <luca@build32.(none)>
2012-11-27 22:30:00 +0100

    Improve the MSR generation script

--- msr.sh
+++ msr.sh
@@ -1,3 +1,15 @@
+GUEST_CONFIG=config-3.0.36-guest-32
+
+while getopts 4 opt
+ do
+  case "$opt" in
+    4)		GUEST_ARCH=x86_64; GUEST_CONFIG=config-3.0.36-guest-64;;
+    [?])	print >&2 "Usage: $0 [-4]"
+		exit 1;;
+  esac
+ done
+
 sh monolithic.sh -4
 sh create_image.sh opt2.img 48
-sh buildclick.sh $(pwd)/core.gz $(pwd)/opt2.img $(pwd)/Configs/config-3.0.36-guest-64 test.img
+export ARCH=$(GUEST_ARCH)
+sh buildclick.sh $(pwd)/core.gz $(pwd)/opt2.img $(pwd)/Configs/$(GUEST_CONFIG) test.img
