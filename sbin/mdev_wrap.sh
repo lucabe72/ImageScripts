9b748ef3fd2c635b7a755bf248632fa4ec61ac63
Luca Abeni <luca.abeni@unitn.it>
2013-01-30 09:32:30 +0100

    Remove some debug

--- sbin/mdev_wrap.sh
+++ sbin/mdev_wrap.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-echo >>/tmp/mdev.trace
-date >>/tmp/mdev.trace
-echo "Params: $*" >>/tmp/mdev.trace
-env | sort >>/tmp/mdev.trace
-
 [ "$ACTION" = add ] && [ "$MODALIAS" != "" ] && /sbin/modprobe $MODALIAS
 [ "$ACTION" = remove ] && [ "$MODALIAS" != "" ] && /sbin/modprobe -r $MODALIAS
 /sbin/mdev $@
