4d3b12bb228b6480b6518ea8f7d311d40cbd6865
Luca Abeni <luca.abeni@unitn.it>
2013-05-14 08:23:20 +0200

    Fix building quagga on 64bit systems
    Install the correct libs in .../lib64
    NOTE: The guest kernel must be 64bit (sh monolithic ... -8)
    NOTE2: Before starting quagga daemons, the guest must do
    	export LD_LIBRARY_PATH=/home/vrouter/Public-Quagga/lib64
           then, /home/vrouter/Public-Quagga/lib64/ld-linux-x86-64.so.2 <program name>...

--- buildquagga.sh
+++ buildquagga.sh
@@ -3,6 +3,7 @@ set -e
 PARTITION_NAME=VRouter
 VRUSER=vrouter
 TMP_DIR=/tmp/Quagga
+TARGET_PATH=/home/$VRUSER/Public-Quagga
 
 . $(dirname $0)/utils.sh
 CPUS=$(get_j)
@@ -35,10 +36,49 @@ install_quagga() {
   cd ..
 }
 
+fetch_lib() {
+  if test -e $1;
+   then
+    LIB=$(find $1 -name $2)
+    cp $LIB $3/$1
+   else
+    echo Fetch Lib: $1 does not exist - doing nothing
+   fi
+}
+
+get_libs64() {
+echo Get libs 64
+  STD_LIBS="libpthread.so libgcc_s.so libc.so librt.so libm.so libdl.so"
+  LD_LINUX=$(strings $1/sbin/zebra | grep ld-linux)
+
+  mkdir -p $1/lib64
+
+  for A in $STD_LIBS
+   do
+    fetch_lib /lib64/ $A* $1
+   done
+
+  cp $LD_LINUX $1/lib64
+
+}
+
 make_quagga() {
   get_quagga quagga-$2
-  build_quagga /home/$VRUSER/Public-Quagga $CPUS quagga-$2
+  build_quagga $TARGET_PATH $CPUS quagga-$2
   install_quagga $1 quagga-$2
+
+  MY_ARCH=$(arch)
+  if [ $MY_ARCH = x86_64 ];
+   then
+    get_libs64 $1$TARGET_PATH
+   else
+#    get_libs $1$TARGET_PATH
+     echo 32bit
+   fi
+  fetch_lib /lib/   libnss_compat* $1$TARGET_PATH
+  fetch_lib /lib/   libnss_files*  $1$TARGET_PATH
+  fetch_lib /lib64/ libnss_compat* $1$TARGET_PATH
+  fetch_lib /lib64/ libnss_files*  $1$TARGET_PATH
 }
 
 update_home() {
