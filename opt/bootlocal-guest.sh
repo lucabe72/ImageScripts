ac018a03be751882c242f2cd3c2adae5464bdbd0
Luca Abeni <luca.abeni@unitn.it>
2013-11-05 08:44:28 +0100

    Allow to configure some additional network parameters
    Patch by Csaba:
    Added gw for default gaway, and a way to specify netmask.
    / notation would be better, but I think this is temporary anyway,
    a better solution based on virtfs and sysfile copying is needed.

--- opt/bootlocal-guest.sh
+++ opt/bootlocal-guest.sh
@@ -5,6 +5,7 @@ for cmd in $CMDLINE
  do
    case $cmd in
      ifaces*) IFACES=${cmd#*=} ;;
+     gw*) GATEWAY=${cmd#*=} ;;
      noroute) NOROUTE=YesPlease ;;
    esac
  done
@@ -15,12 +16,27 @@ IFS=,
 for I in $IFACES
  do
   NAME=$(echo $I | cut -d '-' -f 1)
-  IP=$(echo $I | cut -d '-' -f 2)
+  IPM=$(echo $I | cut -d '-' -f 2)
+  IP=$(echo $IPM | cut -d 'm' -f 1)
+  MASK=$(echo $IPM | cut -d 'm' -f 2)
   /sbin/ifconfig $NAME $IP
+  if [ "x$MASK" != "x" ]
+   then
+    /sbin/ifconfig $NAME netmask $MASK
+  fi
   /sbin/ifconfig $NAME txqueuelen 20000
  done
+
+if [ "x$GW" = "x" ]
+ then
+  route add default gw $GATEWAY
+fi
 
 if [ x$NOROUTE = x ]
  then
   echo 1 > /proc/sys/net/ipv4/ip_forward
- fi
+  cd /home/vrouter
+  Public-Quagga/lib64/ld-linux-x86-64.so.2 --library-path Public-Quagga/lib:Public-Quagga/lib64 Public-Quagga/sbin/zebra -u root -g root -f zebra.conf -d
+  Public-Quagga/lib64/ld-linux-x86-64.so.2 --library-path Public-Quagga/lib:Public-Quagga/lib64 Public-Quagga/sbin/ospfd -u root -g root -f ospfd.conf -d
+fi
+  
