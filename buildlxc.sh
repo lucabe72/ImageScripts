8c17d1945b39af337d26b5f392c8ef1d7f34e565
Luca Abeni <luca@luca-1225C.(none)>
2014-01-03 17:44:35 +0100

    First version of the LXC build scripts

---
+++ buildlxc.sh
@@ -0,0 +1,98 @@
+set -e
+
+PARTITION_NAME=VRouter
+VRUSER=vrouter
+TMP_DIR=/tmp/L
+TARGET_PATH=/home/$VRUSER/Public-LXC
+SITE=http://linuxcontainers.org/downloads/
+
+. $(dirname $0)/utils.sh
+CPUS=$(get_j)
+
+get_lxc() {
+  if test -e $1;
+   then
+    echo $1 already exists
+   else
+    if test -e $1.tar.gz;
+     then
+      echo $1.tar.gz already exists
+     else
+      wget $SITE/$1.tar.gz
+     fi
+    tar xvzf $1.tar.gz
+   fi
+}
+
+build_lxc() {
+  mkdir -p $4
+  cd $4
+  ../$3/configure --prefix=$1 --disable-capabilities --disable-doc
+  make -j $2
+  cd ..
+}
+
+install_lxc() {
+  cd $2
+  make DESTDIR=$1 install
+  strip $1/home/$VRUSER/bin/* || echo cannot strip some scripts - see errors
+  cd ..
+}
+
+make_lxc() {
+  get_lxc     lxc-$2
+  build_lxc   $TARGET_PATH $CPUS lxc-$2 build-lxc-$2
+  install_lxc $1 build-lxc-$2
+}
+
+update_home() {
+  mkdir -p mnt
+  mount_partition $1 img$2 mnt
+
+  if test -e mnt/home;
+   then
+    echo Home already exists, good!
+   else
+    sudo mkdir mnt/home
+   fi
+
+  sudo cp -a $3 mnt/home
+
+  LABEL=$(sudo /sbin/e2label /dev/loop0)
+  if [ x$LABEL = x ]
+   then
+    sudo /sbin/e2label /dev/loop0 $PARTITION_NAME
+   else
+    PARTITION_NAME=$LABEL
+   fi
+  umount_partition mnt
+  rm -rf mnt
+}
+
+add_to_grub() {
+  mkdir mnt
+  mount_partition $1 img1 mnt
+  cp mnt/boot/grub/menu.lst /tmp/GRUB/menu.lst
+  cat >> /tmp/GRUB/menu.lst << EOF
+title		VRouter
+root		(hd0,0)
+kernel		/boot/vmlinuz-$KVER$EXTRAKNAME waitusb=5 nodhcp nozswap opt=LABEL=$PARTITION_NAME user=vrouter home=LABEL=$PARTITION_NAME
+initrd		/boot/core-$KVER$EXTRAKNAME.gz
+EOF
+  sudo cp /tmp/GRUB/menu.lst mnt/boot/grub/menu.lst
+  umount_partition mnt
+  rm -rf mnt
+}
+ 
+
+make_lxc    $TMP_DIR $2
+update_home $1 5 $TMP_DIR/home/$VRUSER
+add_to_grub $1
+
+#wget http://linuxcontainers.org/downloads/lxc-0.9.0.tar.gz
+#tar xvzf lxc-0.9.0.tar.gz
+#cd lxc-0.9.0
+#./configure --prefix=/home/vrouter/LXC --disable-capabilities --disable-doc
+#make -j 3
+#DESTDIR=/tmp/L make install
+#cd ..
---
+++ lxc_host.sh
@@ -0,0 +1,56 @@
+set -e
+
+SDIR=$(cd -- $(dirname $0) && pwd)
+
+. $(dirname $0)/utils.sh
+
+export HOST_ARCH=x86
+export GUEST_ARCH=x86
+CORE=$(pwd)/test.gz
+HOST_KVER=3.12.6
+GUEST_KVER=3.4.14
+EXTRAKNAME="-vrhost"
+KVM_NAME=qemu-kvm-git
+
+while getopts 48c:kv: opt
+ do
+  case "$opt" in
+    4)		HOST_ARCH=x86_64;;
+    8)		GUEST_ARCH=x86_64;;
+    c)		CORE=$OPTARG;;
+    k)		KEEPIMAGE=YesPlease;;
+    v)		HOST_KVER=$OPTARG;;
+    [?])	print >&2 "Usage: $0 [-4]"
+		exit 1;;
+  esac
+ done
+
+if test -e $CORE;
+ then
+  echo $CORE already exists
+ else
+  echo Building $CORE
+  sh $SDIR/buildcore.sh $CORE
+ fi
+
+export EXTRAKNAME
+if [ x$KEEPIMAGE = x ]
+ then
+  sh $SDIR/create_image.sh test.img 512 32
+  sh $SDIR/grubit.sh test.img
+ fi
+sh $SDIR/create_image.sh opt1.img 48
+export KVER=$GUEST_KVER
+export ARCH=$GUEST_ARCH
+GUEST_CONFIG=$(get_kernel_config_name $GUEST_KVER $GUEST_ARCH guest)
+sh $SDIR/buildquagga.sh opt1.img 0.99.22
+sh $SDIR/buildguest.sh $CORE $SDIR/Configs/$GUEST_CONFIG opt1.img test.img
+#sh $SDIR/installguest.sh opt1.img $PWD/Out/Guest test.img
+export KVER=$HOST_KVER
+export ARCH=$HOST_ARCH
+HOST_CONFIG=$(get_kernel_config_name $HOST_KVER $HOST_ARCH host)
+sh $SDIR/buildhostlin.sh  $CORE $SDIR/Configs/$HOST_CONFIG test.img
+sh $SDIR/buildlxc.sh      test.img 0.9.0 
+sh $SDIR/buildovs.sh      test.img 2.0.0 
+sh $SDIR/buildiproute2.sh test.img 3.12.0
+sh $SDIR/buildethtool.sh  test.img 3.12.1
